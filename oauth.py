ec42cb96b1b1b1b08d4f48dc7802ead1a2736d23
Ashton Jordan <ashjorda@cisco.com>
2024-04-17 21:39:01 -0500

    Removed unnecessary packages, and calls within the code. And changed the granted.html page H1 header.

--- oauth.py
+++ oauth.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, session
+from flask import Flask, render_template, jsonify
 import requests
 import time
 from threading import Thread
@@ -11,23 +11,23 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
 # Global storage for the access token
-access_token_info = {
+session = {
     'access_token': None,
-    'token_ready': False
+    'token_ready': False,
+    'device_code': None
 }
 
 # Define your clientID and client secret
 clientID = os.environ['clientID']
 secretID = os.environ['secretID']
 credentials = os.environ['credentials']
-redirectURI = "http://0.0.0.0:10060/helperservice/v1/actions/device/callback"  # This could be different if you publicly expose this endpoint.
 
 
 @app.route("/")
 def main_page():
     """Main Grant page"""
     scopes = "meeting:recordings_read spark:all spark:kms cjp:config_write cjp:config cjds:admin_org_read cjds:admin_org_write"
-    params = {'client_id': clientID, 'scope': scopes, 'redirectURI': redirectURI}
+    params = {'client_id': clientID, 'scope': scopes}
     devices_api = "https://webexapis.com/v1/device/authorize"
     api_response = requests.post(url=devices_api, data=params)
     response_json = api_response.json()
@@ -57,14 +57,9 @@ def qr_cde_generation(url):
 
 
 def poll_for_access_token(device_code):
-    print("Inside of poll for access token")
-    print(device_code)
     """Poll the token endpoint for an access token."""
     token_url = "https://webexapis.com/v1/device/token"
-    headers = {'Authorization': f'Basic {credentials}', 'Accept': 'application/json', 'ContentType': 'application/x'
-                                                                                                     '-www-form'
-                                                                                                     '-urlencoded; '
-                                                                                                     'charset=utf-8'}
+    headers = {'Authorization': f'Basic {credentials}'}
     body = {
         'client_id': f'{clientID}',
         'device_code': f'{device_code}',
@@ -78,32 +73,29 @@ def poll_for_access_token(device_code):
         if token_response.status_code == 200:
             access_token = token_response.json()['access_token']
             # Store the access token in session or another secure place
-            # session['access_token'] = access_token
-            access_token_info['access_token'] = access_token
-            access_token_info['token_ready'] = True
+            session['access_token'] = access_token
+            session['token_ready'] = True
             print("Access token obtained")
             break
-        elif token_response.json().get('error') == 'authorization_pending':
-            # User has not completed the authentication yet; continue polling
-            time.sleep(2)
         else:
             # Handle other errors (e.g., 'slow_down', 'expired_token')
-            print("Error polling for token:", token_response.json())
+            print("Response Code:", token_response.status_code, token_response.json()['errors'][0]['description'])
 
 
-# This route is just an example to show how to access the stored access token
 @app.route("/granted")
 def granted():
     # Check if the access token is ready and render the granted.html template
-    if access_token_info['token_ready']:
-        return render_template("granted.html", access_token=access_token_info['access_token'])
+    if session['token_ready']:
+        return render_template("granted.html", access_token=session['access_token'])
     else:
         # If the token isn't ready, you might want to inform the user or redirect
         return "Access token not ready yet. Please try again later."
 
+
 @app.route("/access_token_ready")
 def access_token_ready():
-    return jsonify({'token_ready': access_token_info['token_ready']})
+    return jsonify({'token_ready': session['token_ready']})
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=10060)
